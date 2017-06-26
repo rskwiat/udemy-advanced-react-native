220963397c6526d29a2df526d5a0109832feac08
Robert Skwiat <rskwiat@gmail.com>
2017-06-26 17:36:33 -0400

    working google functions

--- one-time-pw/functions/create_user.js
+++ one-time-pw/functions/create_user.js
@@ -1,3 +1,14 @@
+//import service account
+const admin = require('firebase-admin');
+
 module.exports = function(req, res) {
-  res.send(req.body);
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'Bad Input' });
+  }
+
+  const phone = String(req.body.phone).replace(/[^\d]/g, "");
+
+  admin.auth().createUser({ uid: phone })
+    .then(user => res.send(user))
+    .catch(err => res.status(422).send({ error: err }));
 }
